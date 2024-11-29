9db87cc2c3f7f6a020ed7b891912f9675d800e55
matthewliii <matthewli4859@gmail.com>
2024-11-29 16:00:15 -0500

    yml initialized

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from lxml import html
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
-
 
 # Making a GET request
 r = requests.get('https://connect2concepts.com/connect2/?type=circle&key=2A2BE0D8-DF10-4A48-BEDD-B3BC0CD628E7', headers={"User-Agent": "XY"})
@@ -22,15 +17,25 @@ for s in text:
         clean.append(s)
 for i in clean:
     print(i)
-# # print(soup)
-
-s = soup.findAll('div')
-# d = s.find('document')
-# print(s)
 
 
+# def main(data, context):
+#     try:
+#         # Making a GET request
+#         r = requests.get('https://connect2concepts.com/connect2/?type=circle&key=2A2BE0D8-DF10-4A48-BEDD-B3BC0CD628E7', headers={"User-Agent": "XY"})
 
-# soup2 = BeautifulSoup(s.content, 'html.parser')
+#         # check status code for response received
+#         # success code - 200
+#         print(r.status_code)
 
-# content = s.find_all('span')
-# print(content)
+#         # Parsing the HTML
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         text = soup.get_text().splitlines()
+#         clean = []
+#         for s in text:
+#             if len(s) > 30 and (s[0] == 'M' or s[0] == 'S'):
+#                 clean.append(s)
+#     except Exception as error:
+#         log_message = Template('$error').substitute(error=error)
+#         logging.error(log_message)
+# if
