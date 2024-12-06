0694e7bc36948718d5d8460f2efdeb8d58fed033
matthewliii <matthewli4859@gmail.com>
2024-12-06 16:35:55 -0500

    cron time change

--- graph.py
+++ graph.py
@@ -38,4 +38,4 @@ def graph(location, day):
     plt.tight_layout()
     plt.show()
 
-graph("Marino Center - 3rd Floor Weight Room", "Sunday")
+graph("Marino Center - 3rd Floor Weight Room", "Wednesday")
